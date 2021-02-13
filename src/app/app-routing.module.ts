3ac5eb5c4b09b8302bfe128fa7ee1f8d493da4e2
Masood, Raas <Raas.Masood@insight.com>
2021-02-13 02:03:43 -0600

    Setting up Rounting and Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DepositFundsComponent } from './deposit-funds/deposit-funds.component';
+import { ManageAccountsComponent } from './manage-accounts/manage-accounts.component';
+import { OpenAccountComponent } from './open-account/open-account.component';
+import { TransferFundsComponent } from './transfer-funds/transfer-funds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'manage-accounts', component: ManageAccountsComponent },
+  { path: 'open-account', component: OpenAccountComponent },
+  { path: 'transfer-funds', component: TransferFundsComponent },
+  { path: 'deposit-funds', component: DepositFundsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { ManageAccountsComponent } from './manage-accounts/manage-accounts.component';
+import { TransferFundsComponent } from './transfer-funds/transfer-funds.component';
+import { DepositFundsComponent } from './deposit-funds/deposit-funds.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OpenAccountComponent } from './open-account/open-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageAccountsComponent,
+    TransferFundsComponent,
+    DepositFundsComponent,
+    DashboardComponent,
+    OpenAccountComponent
   ],
   imports: [
     BrowserModule,
